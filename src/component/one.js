5275adc73ed5ade87d0252d1efcd5ed0b62a47af
Your Name <you@example.com>
2019-09-14 08:53:02 +0700

    sss

--- src/component/one.js
+++ src/component/one.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
-const one = (props) => <div>one 11111111111111111111111111111111111111113233331</div>
-=======
-const one = (props) => <div>one 1</div>
->>>>>>> parent of efbea7e... 1111111111
+const one = (props) => <div>one 123</div>
 
 export default one;
